13f97b2283a24f6759c2787d7a2d5e2ef703cce1
uyukigakata <c1204269@st.kanazawa-it.ac.jp>
2024-09-23 19:56:02 +0900

    チャット

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default async function Home() {//サイトindex
+  return (
+    // <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pb-16 pt-20 text-center lg:pt-32">
+    //   <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
+    //     Hello
+    //     <span className="relative whitespace-nowrap text-blue-600"> World</span>
+    //   </h1>
+    // </main>
+    <div className="flex-1 w-full flex flex-col items-center">
+      <h1 className="text-3xl font-bold pt-6 pb-10">認証機能ありリアルタイムチャットアプリ</h1>
+    </div>
+  )
+}
